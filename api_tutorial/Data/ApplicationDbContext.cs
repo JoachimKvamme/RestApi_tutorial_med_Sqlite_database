f3e174ec5299aa83ebba3614918485765ba5a5d0
JoachimKvamme <kaj082024@gmail.com>
2024-10-15 11:37:47 +0200

    initial commit

---
+++ api_tutorial/Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using api_tutorial.Models;
+
+namespace api_tutorial.Data
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext(DbContextOptions dbContextOptions)
+        : base(dbContextOptions)
+        {
+            
+        }
+        public DbSet<Stock> Stock {get; set;}
+        public DbSet<Comment> Comments {get; set;}
+    }
+}
